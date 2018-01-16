4d2e6632a7e5da020e0abcdefef2a303ed4e4d1d
Cameron Green <cgree78@gmail.com>
2018-01-15 22:53:11 -0600

    finally got fonts working

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,14 +4,21 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-sass`, {
+    `gatsby-plugin-sass`,
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `data`,
         path: `${__dirname}/src/data`
       }
     },
-    `gatsby-transformer-json`
+    `gatsby-transformer-json`,
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`Roboto`, `source sans`]
+      }
+    }
   ],
 
   pathPrefix: `/Kaijuwu`
