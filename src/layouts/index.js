ecc73c7d0636b970d43bed1bad89e2a189ee0fdd
Cameron Green <cgree78@gmail.com>
2018-01-15 20:15:03 -0600

    added genji to background image

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,42 +1,51 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Link from 'gatsby-link'
-import Helmet from 'react-helmet'
+import React from "react"
+import PropTypes from "prop-types"
+import Link from "gatsby-link"
+import Helmet from "react-helmet"
 
-import './index.scss'
+import "./index.scss"
 
 const Header = () => (
-  <div style={{
-    marginBottom: '1.45rem'
-  }}>
+  <div
+    style={{
+      marginBottom: "1.45rem"
+    }}
+  >
     <div
       className="header"
       style={{
-      margin: '0 auto',
-      maxWidth: 960,
-      padding: '1.45rem 1.0875rem'
-    }}>
-      <h4 className="headerIcon" style={{
-      }}>
-        <Link to="/" style={{
-          textDecoration: 'none'
-        }}>
+        margin: "0 auto",
+        maxWidth: 960,
+        padding: "1.45rem 1.0875rem"
+      }}
+    >
+      <h4 className="headerIcon" style={{}}>
+        <Link
+          to="/"
+          style={{
+            textDecoration: "none"
+          }}
+        >
           Home
         </Link>
       </h4>
-      <h4 style={{
-      }}>
-        <Link to="/About/" style={{
-          textDecoration: 'none'
-        }}>
+      <h4 style={{}}>
+        <Link
+          to="/About/"
+          style={{
+            textDecoration: "none"
+          }}
+        >
           About
         </Link>
       </h4>
-      <h4 style={{
-      }}>
-        <Link to="/Teams/" style={{
-          textDecoration: 'none'
-        }}>
+      <h4 style={{}}>
+        <Link
+          to="/Teams/"
+          style={{
+            textDecoration: "none"
+          }}
+        >
           Teams
         </Link>
       </h4>
@@ -44,27 +53,30 @@ const Header = () => (
   </div>
 )
 
-const TemplateWrapper = ({children}) => (
+const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
       title="KAIJUWU Heavy Industries"
       meta={[
-      {
-        name: 'description',
-        content: 'Sample'
-      }, {
-        name: 'keywords',
-        content: 'sample, something'
-      }
-    ]}/>
-    <Header/>
+        {
+          name: "description",
+          content: "Sample"
+        },
+        {
+          name: "keywords",
+          content: "sample, something"
+        }
+      ]}
+    />
+    <Header />
     <div
       style={{
-      margin: '0 auto',
-      maxWidth: 960,
-      padding: '0px 1.0875rem 1.45rem',
-      paddingTop: 0
-    }}>
+        margin: "0 auto",
+        maxWidth: 960,
+        padding: "0px 1.0875rem 1.45rem",
+        paddingTop: 0
+      }}
+    >
       {children()}
     </div>
   </div>
