28582c830bb2336e9b8bb8194f9f6af594180f7b
Cameron Green <cgree78@gmail.com>
2018-02-02 13:06:22 -0600

    added "about" to the main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,41 @@ const IndexPage = () => (
       <h1>KAIJUWU</h1>
       <h1>Heavy</h1>
       <h1>Industries</h1>
+
+      <div
+    className="aboutDiv textShadow"
+    style={{
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignContent: "center"
+      // alignItems: "center",
+    }}
+  >
+    {/* <h1
+      style={{
+        margin: "auto",
+        color: "white",
+        padding: "1rem"
+      }}
+    >
+      About
+    </h1> */}
+    <br />
+    <div className="aboutDiv_info">
+      <p>Havin' fun and gettin' gud. </p>
+      <p>
+        Or something like that. KAIJUWU HEAVY INDUSTRIES is a group of
+        invite-only casual/competitive teams (mainly playing Overwatch) that put
+        an emphasis on chemistry and community first.
+      </p>
+      <p>
+        KHI aims to foster relational teams that value each member for more than
+        just their skills, and provide the tools and assistance to help each
+        team improve. We value positivity, responsibility, and respectfulness.
+      </p>
+    </div>
+  </div>
     </div>
   </div>
 )
