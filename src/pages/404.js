9e8dfcdb85bd3118e7f61532d2a7095f4b8a6976
Cameron Green <Ecleptic@users.noreply.github.com>
2019-02-01 16:29:40 -0600

    Changed the 404 message

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react"
 
 const NotFoundPage = () => (
   <div>
     <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <p>
+      You just hit a route that doesn&#39;t exist... If you feel like there
+      should be something here, please message either Tengu or Ecleptic at the
+      bottom of the page!
+    </p>
   </div>
 )
 
